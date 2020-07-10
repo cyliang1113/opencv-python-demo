83318f2ef2964bfde012f594af5e63865d150c35
leon <cyliang1113@126.com>
2020-07-10 22:49:58 +0800

    11111

---
+++ leolab/opencv/demo/threshold/demo02_threshold_adapt.py
@@ -0,0 +1,30 @@
+# 图像就是像素的数组
+
+
+import cv2
+
+# 自适应阈值处理
+
+img_file = "../gonglu.jpg"
+
+img = cv2.imread(img_file, 0)
+cv2.imshow("img", img)
+print("img: \n", img)
+
+# 阈值125 大于阈值的变成255, 小于阈值的变成0
+t, rst = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY)
+cv2.imshow("rst", rst)
+print("rst: \n", rst)
+
+# 自适应阈值 小范围内有一个阈值
+adaptMean = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 3)
+cv2.imshow("adaptMean", adaptMean)
+print("adaptMean: \n", adaptMean)
+
+adaptGauss = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 3)
+cv2.imshow("adaptGauss", adaptGauss)
+print("adaptGauss: \n", adaptGauss)
+
+cv2.waitKey()
+
+cv2.destroyAllWindows()
---
+++ leolab/opencv/demo/threshold/demo03_threshold_otsu.py
@@ -0,0 +1,31 @@
+# 图像就是像素的数组
+
+
+import cv2
+
+# 阈值处理 自动计算合适的阈值
+
+img_file = "../jay.jpg"
+# img_file = "E:\\_leo\\666.jpg"
+
+img = cv2.imread(img_file, 0)
+cv2.imshow("img", img)
+
+
+# 阈值125 大于阈值的变成255, 小于阈值的变成0
+t, rst = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY)
+cv2.imshow("rst", rst)
+
+
+# 自动计算合适的阈值
+threshold, otsu = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+cv2.imshow("otsu", otsu)
+print("threshold: \n", threshold)
+
+# 自适应阈值
+adaptGauss = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 3)
+cv2.imshow("adaptGauss", adaptGauss)
+
+cv2.waitKey()
+
+cv2.destroyAllWindows()
