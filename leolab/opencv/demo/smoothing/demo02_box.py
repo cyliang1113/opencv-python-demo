1c8e6f660c78f591b0b017eaa08a98044be13943
cyliang1113 <cyliang1113@126.com>
2020-07-11 13:39:55 +0800

    1111

--- leolab/opencv/demo/smoothing/demo02_box.py
+++ leolab/opencv/demo/smoothing/demo02_box.py
@@ -10,10 +10,10 @@ img = cv2.imread(img_file, 0)
 cv2.imshow("img", img)
 
 # 方框滤波
-boxFilter = cv2.boxFilter(img, -1, (3, 3)) # normalize默认是1, 归一化 平均值 和均值滤波一样
+boxFilter = cv2.boxFilter(img, -1, (3, 3))  # normalize默认是1, 归一化 平均值 和均值滤波一样
 cv2.imshow("boxFilter", boxFilter)
 
-boxFilter1 = cv2.boxFilter(img, -1, (3, 3), normalize=0) # 不求平均值
+boxFilter1 = cv2.boxFilter(img, -1, (3, 3), normalize=0)  # 不求平均值
 cv2.imshow("boxFilter1", boxFilter1)
 
 cv2.waitKey()
