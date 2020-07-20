3938d482ca080e819dd0c9140149bf01d0dce185
cyliang1113 <cyliang1113@126.com>
2020-07-20 14:03:55 +0800

    视频读取

---
+++ leolab/opencv/demo/video/demo01_camera_read.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+# 打开电脑摄像头
+capture = cv2.VideoCapture(0)
+
+print("capture: ", capture)
+
+i = 0
+while (capture.isOpened()):
+    i = i + 1
+    input("按回车键")
+    ret, frame = capture.read()
+    print("capture read ret: ", ret)
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    frame_gray_write_ret = cv2.imwrite("D:\\capture\\capture_gray_" + str(i) + ".jpg", frame_gray)
+    print("frame_gray write ret: ", frame_gray_write_ret)
+
+    t, frame_bin = cv2.threshold(frame_gray, 125, 255, cv2.THRESH_BINARY)
+
+    frame_bin_adapt = cv2.adaptiveThreshold(frame_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 3)
+
+    frame_bin_ret = cv2.imwrite("D:\\capture\\capture_bin_" + str(i) + ".jpg", frame_bin)
+    print("frame_bin write ret: ", frame_bin_ret)
+
+    frame_bin_adapt_ret = cv2.imwrite("D:\\capture\\capture_bin_adapt_" + str(i) + ".jpg", frame_bin_adapt)
+    print("frame_bin_adapt  write  ret: ", frame_bin_adapt_ret)
