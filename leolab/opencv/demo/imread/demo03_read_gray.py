b968da8cae8434503f86ba0d3a87d493b2945600
leon <cyliang1113@126.com>
2020-07-21 19:29:01 +0800

    灰度读取

--- leolab/opencv/demo/imread/demo03_read_gray.py
+++ leolab/opencv/demo/imread/demo03_read_gray.py
@@ -5,8 +5,10 @@ import cv2
 # 读图片, 返回 像素数组
 
 img_file = "../image_src/xiaoxin.jpg"
+img = cv2.imread(img_file)
+cv2.imshow("img", img)
 
-# 灰度处理
+# 灰度读
 img1 = cv2.imread(img_file, 0)
 cv2.imshow("img1", img1)
 
