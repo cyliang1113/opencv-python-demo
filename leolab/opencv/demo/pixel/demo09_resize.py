baf1e553b95b4161e259592cf512e912346b62c7
leon <cyliang1113@126.com>
2021-01-02 13:13:50 +0800

    11111

---
+++ leolab/opencv/demo/pixel/demo09_resize.py
@@ -0,0 +1,15 @@
+
+
+# 图像就是像素的数组
+
+import cv2
+import numpy as np
+
+img_file = "D:\\6.png"
+img = cv2.imread(img_file, 0)
+img2 = cv2.resize(img, (28, 28), interpolation = cv2.INTER_AREA)
+print(img2.shape)
+
+cv2.imshow("img2", img2)
+# 等待按键
+key = cv2.waitKey()
